b1cf62a0f1c2c712101ef9cb409d91ddea7c08a7
alimirza <ali8751805@gmail.com>
2024-10-20 19:37:38 +0500

    Remove some files

--- type_casting.py
+++
@@ -1,5 +0,0 @@
-name = input("Enter name")
-id = input("Enter ID")
-
-print("Welcome", name)
-print("Your Id is", int(id))
--- variables.py
+++
@@ -1,3 +0,0 @@
-a=input("Enter value of a")
-
-print("The value of a is",a)
